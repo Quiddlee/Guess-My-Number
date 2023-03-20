65c0c6f716790f38088311c6952e7760da30d272
Bogdan <thegarrybit@gmail.com>
2023-03-20 21:27:28 +0200

    feat: add play again and local storage for high score

--- app.js
+++ app.js
@@ -6,30 +6,49 @@ document.addEventListener('DOMContentLoaded', () => {
   const number = document.querySelector('.number');
   const scoreElem = document.querySelector('.score');
   const highScoreElem = document.querySelector('.label-highscore');
+  const againBtn = document.querySelector('.again');
+  const userInput = guessBtn.previousElementSibling;
+  const body = document.querySelector('body');
   let score = +scoreElem.textContent;
-  let highScoreNum = 0;
   let guessingNum = Math.floor(Math.random() * 20);
 
-  guessBtn.addEventListener('click', () => {
-    const userInput = +guessBtn.previousElementSibling.value;
-    console.log(userInput, guessingNum);
+  if (localStorage.getItem('highScore')) {
+    highScoreElem.firstElementChild.textContent = localStorage.getItem(
+        'highScore');
+  }
 
-    if (guessingNum === userInput) {
-      document.querySelector('body').style.backgroundColor = '#60b347';
+  guessBtn.addEventListener('click', () => {
+    const userVal = +userInput.value;
+    console.log(userVal, guessingNum);
+
+    if (guessingNum === userVal) {
+      body.style.backgroundColor = '#60b347';
       message.textContent = '🎉 Correct Number!';
       number.textContent = guessingNum;
-      highScoreElem.textContent = `🥇 Highscore: ${ score > highScoreNum ?
-                                                   score :
-                                                   highScoreNum }`;
+
+      const highScore = highScoreElem.firstElementChild;
+      if (score > +highScore.textContent) {
+        highScore.textContent = score;
+        localStorage.setItem('highScore', score);
+      }
     }
     else {
       scoreElem.textContent = --score;
-      if (userInput > guessingNum) {
+      if (userVal > guessingNum) {
         message.textContent = '📈 Too high!';
       }
       else {
         message.textContent = '📉 Too low!';
       }
     }
+  });
+
+  againBtn.addEventListener('click', () => {
+    body.style.backgroundColor = '#222';
+    userInput.value = '';
+    scoreElem.textContent = 20;
+    score = 20;
+    message.textContent = 'Start guessing...';
+    number.textContent = '?';
   });
 });
